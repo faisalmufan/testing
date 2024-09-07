237eaecd201a9f5e96adb03044007c8fb7e4215c
wisnu <wisnu@rumahningrat.id>
2024-09-07 07:27:15 +0000

    test

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -85,6 +85,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<span>Testing IT - Faisal</span>
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
+		<span>Commit Lagi - Faisal</span>
+
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
 
